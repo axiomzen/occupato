7699f3871db75ff8c2edd4476522cfeb92e8ef04
Daniel Gipps <daniel@axiomzen.co>
2014-11-22 16:51:40 -0800

    Basic index and get update endpoint working

---
+++ models/room.js
@@ -0,0 +1,39 @@
+module.exports = function() {
+
+  fns = {
+    name: "",
+    empty: true,
+
+    setName: function(room_name) {
+      name = room_name;
+    },
+    getName: function() {
+      return name;
+    },
+    setStatus: function(is_empty) {
+      console.log(is_empty);
+      if (is_empty) {
+        console.log("about to check if empty");
+        if (is_empty === "1") {
+          this.empty = false;
+        } else {
+          this.empty = true;
+        }
+      }
+    },
+    getStatus: function() {
+      return this.empty;
+    },
+    getStatusString: function() {
+      if (this.empty) {
+        return "Empty!";
+      } else {
+        return "Occupied!";
+      }
+    }
+  }
+
+  return fns;
+
+
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+var mens = require('../models/room')();
+var womens = require('../models/room')();
+var shower = require('../models/room')();
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  console.log(req.query);
+  if (req.query.id) {
+    switch (req.query.id) {
+      case "1":
+        mens.setStatus(req.query.open);
+        break;
+      case "2":
+        womens.setStatus(req.query.open);
+        break;
+      case "3":
+        shower.setStatus(req.query.open);
+        break;
+    }
+  }
+  res.send({});
+});
+
+router.get('/status', function(req, res) {
+  res.render('status', {
+    title: 'Bathroom Status',
+    mens: mens.getStatusString(),
+    womens: womens.getStatusString(),
+    shower: shower.getStatusString()
+    });
 });
 
 module.exports = router;
