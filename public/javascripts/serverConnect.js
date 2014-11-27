6c2aced636183d873293830f8039a599a331f7cb
Daniel Gipps <daniel@axiomzen.co>
2014-11-27 15:36:42 -0800

    Fixed Geoffrey's missing javascript

--- public/javascripts/serverConnect.js
+++ public/javascripts/serverConnect.js
@@ -3,6 +3,21 @@ var socket = io();
 socket.on('update', function (data) {
   console.log(data);
   $("#mens").text(data.mens);
+  if (data.mens === "occupied") {
+    $(".occupato-mens").addClass("occupato-occupied");
+  } else {
+    $(".occupato-mens").removeClass("occupato-occupied");
+  }
   $("#womens").text(data.womens);
+  if (data.womens === "occupied") {
+    $(".occupato-womens").addClass("occupato-occupied");
+  } else {
+    $(".occupato-womens").removeClass("occupato-occupied");
+  }
   $("#shower").text(data.shower);
+  if (data.shower === "occupied") {
+    $(".occupato-shower").addClass("occupato-occupied");
+  } else {
+    $(".occupato-shower").removeClass("occupato-occupied");
+  }
 });
