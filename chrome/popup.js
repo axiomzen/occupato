d09c653e794573f1eff2dd55a09d06c053d8b715
Geoffrey Tisserand <geoffrey.tisserand@gmail.com>
2015-02-23 17:29:39 -0800

    Add status as a badge, able to change status

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,1 +1,29 @@
-console.log('Popup open');
+$(document).ready(function() {
+  $('.mens').click(function(ev) {
+    broadcastChange('mens');
+  });
+
+  $('.womens').click(function(ev) {
+    broadcastChange('womens');
+  });
+
+  $('.shower').click(function(ev) {
+    broadcastChange('shower');
+  });
+
+  function broadcastChange(newFavoriteBathroom) {
+    chrome.runtime.sendMessage({
+      action: 'changeFavoriteBathroom',
+      bathroom: newFavoriteBathroom
+    });
+  }
+
+
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === 'statusChanged') {
+      console.log('RECEIVED NEW STATUS', request.bathroomStatus);
+      $('.status').removeClass('occupied');
+    }
+  });
+
+});
